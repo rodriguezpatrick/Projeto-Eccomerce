29ba1a1e02d7ebc413bfae174d159f237ada4c07
Patrick Rodrigues <patrickrodrigues740@gmail.com>
2024-08-16 11:27:10 -0300

    página da loja com responsividade feito

--- js/loja.js
+++ js/loja.js
@@ -8,3 +8,28 @@ eleFiltrarExpansivel.forEach(function (ele){
 ele.parentElement.classList.toggle('filtrar__expansivel-menu--aberto');
 });
 });
+
+// script do filtrar
+
+// eleFiltrarFechar = document.querySelector('.filtrar__fechar')
+// eleBody = document.getElementsByTagName('body')
+
+// eleFiltrarFechar.addEventListener('click', function(){
+//   eleBody.toggle('filtrar-menu');
+// })
+
+// como eu vou fazer para o toggle pegar a classe exatamente do body?
+// O de cima está errado não sei por qual motivo. Eu ainda nao sei pensar como um desenvolvedor javascrip, então não faço ideia de como fazer esse código de fechar
+
+// Aqui está o correto
+
+var elmFecharFiltro = document.querySelector(".filtrar__fechar");
+var elmAbrirFiltro = document.querySelector(".produtos__botao");
+
+elmFecharFiltro.addEventListener("click", function(){
+  document.body.classList.remove('filtrar-menu');
+});
+
+elmAbrirFiltro.addEventListener("click", function(){
+  document.body.classList.add("filtrar-menu");
+});
